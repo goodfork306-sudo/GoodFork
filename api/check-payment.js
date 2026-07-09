9040e892b01a6cc81392e381f58edfba18680071
goodfork306-sudo <goodfork306@gmail.com>
2026-07-09 19:28:12 +0200

    Login

--- api/check-payment.js
+++ api/check-payment.js
@@ -11,6 +11,7 @@ async function redisGet(key) {
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   const { session } = req.query;
